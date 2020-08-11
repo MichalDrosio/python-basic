75b7514994349209d63d318b61cc5dcd71e88e2a
mordziak <drosio.michal@gmail.com>
2020-08-11 15:08:16 +0200

    podstawy

---
+++ wyrażenia regularne/phon_number.py
@@ -0,0 +1,17 @@
+def phone_number(selfphone):
+    if len(selfphone) != 11:
+        return False
+    for i in range(0, 3):
+        if not selfphone[i].isdecimal():
+            return False
+    if selfphone[3] != '-' or selfphone[7] != '-':
+        return False
+    for i in range(4, 7):
+        if not selfphone[i].isdecimal():
+            return False
+    for i in range(8, 11):
+        if not selfphone[i].isdecimal():
+            return False
+    return selfphone
+
+print(phone_number('668-983-526'))
