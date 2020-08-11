b4dd5d849cf0f736c8bcaeff593a5e0ca3a7932f
mordziak <drosio.michal@gmail.com>
2020-08-11 14:51:45 +0200

    podstawy

---
+++ kółko_i_krzyżyk.py
@@ -0,0 +1,30 @@
+the_board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+             'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+def print_board(board):
+    print(f'{board["top-L"]} | {board["top-M"]} | {board["top-R"]}')
+    print('--+---+--')
+    print(f'{board["mid-L"]} | {board["mid-M"]} | {board["mid-R"]}')
+    print('--+---+--')
+    print(f'{board["low-L"]} | {board["low-M"]} | {board["low-R"]}')
+
+for k, v in the_board.items():
+    print(f'{k}',end=', ')
+print()
+
+
+turn = 'X'
+for i in range(9):
+    print_board(the_board)
+    print(f"Ruch gracza {turn}. W którym poku stawiasz znak")
+    move = input()
+    the_board[move] = turn
+    if turn == 'X':
+        turn = "O"
+    else:
+        turn = "X"
+
+
+print_board(the_board)
+
