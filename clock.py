97c4d6828049128cf80292b5872d0981dc44c609
MichalDrosio <drosio.michal@gmail.com>
2020-09-23 14:32:36 +0200

    clock

---
+++ clock.py
@@ -0,0 +1,49 @@
+import time
+
+class Clock:
+    def __init__(self, seconds, minutes, hour, mode):
+        self.seconds = seconds
+        self.minutes = minutes
+        self.hour = hour
+        self.mode = mode
+
+    def show_clock(self):
+        while True:
+            print(f'{self.hour}:{self.minutes}:{self.seconds}')
+            self.seconds += 1
+            time.sleep(1)
+            if self.seconds == 60:
+                self.minutes += 1
+                self.seconds = 0
+                if self.minutes == 60:
+                    self.hour += 1
+                    self.minutes = 0
+                    if self.mode == 24:
+                        if self.hour == 24:
+                            self.hour = 0
+                        elif self.mode == 12:
+                            if self.hour == 12:
+                                self.hour = 0
+    
+    
+class Clock_24(Clock):
+    def __init__(self, seconds, minutes, hour, mode):
+        super(Clock_24, self).__init__(seconds, minutes, hour, mode)
+
+
+class Clock_12(Clock):
+        def __init__(self, seconds, minutes, hour, mode):
+            super(Clock_12, self).__init__(seconds, minutes, hour, mode)
+
+
+# c24 = Clock_24(55, 59, 23, 24)
+# print(c24.show_clock())
+# print('---------------------------------------------------------')
+c12 = Clock_12(55, 59, 11, 12)
+print(c12.show_clock())
+
+
+
+
+
+
