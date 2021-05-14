199bf6e9547e8a92b1929866c19d2dc2de217e67
MichalDrosio <drosio.michal@gmail.com>
2021-05-14 13:55:28 +0200

    map_over_a_list_of_lists

---
+++ CodersWars/map_over_a_list_of_lists.py
@@ -0,0 +1,29 @@
+# Write a function which maps a function over the lists in a list:
+# def grid_map(inp, op)
+# # which performs op(element) for all elements of inp
+# Example
+# 1:
+#
+# x = [[1, 2, 3],
+#      [4, 5, 6]]
+#
+# grid_map(x, lambda x: x + 1)
+# -- returns[[2, 3, 4], [5, 6, 7]]
+#
+# grid_map(x, lambda x: x ** 2)
+# -- returns[[1, 4, 9], [16, 25, 36]]
+# Example
+# 2
+#
+# x = [['h', 'E', 'l', 'l', 'O'], ['w', 'O', 'r', 'L', 'd']]
+# grid_map(x, lambda x: x.upper())
+# -- returns[['H', 'E', 'L', 'L', 'O'], ['W', 'O', 'R', 'L', 'D']]
+
+def grid_map(inp, op):
+    return [list(map(op, x)) for x in inp]
+
+
+num_grid = [[1, 2, 3], [4, 5, 6]]
+char_grid = [['h', 'E', 'l', 'l', 'O'], ['w', 'O', 'r', 'L', 'd']]
+
+print(grid_map(num_grid, lambda x: x + 1))
