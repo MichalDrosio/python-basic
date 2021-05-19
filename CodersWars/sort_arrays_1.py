84e76f655e3a02e2a90b4b5cc79710738024d5dd
MichalDrosio <drosio.michal@gmail.com>
2021-05-19 12:58:12 +0200

    sort_arrays_1

---
+++ CodersWars/sort_arrays_1.py
@@ -0,0 +1,7 @@
+# Just a simple sorting usage.
+# Create a function that returns the elements of the input-array / list in a sorted manner.
+
+def sortme(names):
+    return sorted(names)
+
+print(sortme(["one", "two", "three"]))
