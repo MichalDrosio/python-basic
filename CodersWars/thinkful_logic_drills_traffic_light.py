e0e413a1296a31c193950af990afa675f1fd3f0a
MichalDrosio <drosio.michal@gmail.com>
2021-06-08 15:13:07 +0200

    thinkful_dictionary_drills_order_filler

---
+++ CodersWars/thinkful_logic_drills_traffic_light.py
@@ -0,0 +1,20 @@
+# You're writing code to control your town's traffic lights. You need a function to handle each change from green,
+# to yellow, to red, and then to green again.
+#
+# Complete the function that takes a string as an argument representing
+# the current state of the light and returns a string representing the state the light should change to.
+#
+# For example, update_light('green') should return 'yellow'.
+
+def update_light(current):
+    colors = ('green', 'yellow', 'red')
+    color = colors.index(current)
+    if color == len(colors)-1:
+        return colors[0]
+    else:
+        return colors[color+1]
+
+
+print(update_light('green'))
+print(update_light('yellow'))
+print(update_light('red'))
