824b7d99f29ef53035b9e097cc548faaf830ac77
MichalDrosio <drosio.michal@gmail.com>
2021-06-21 09:36:35 +0200

    back_to_forth_then_reverse

---
+++ CodersWars/back_to_forth_then_reverse.py
@@ -0,0 +1,47 @@
+# A list S will be given. You need to generate a list T from it by following the given process:
+#
+# Remove the first and last element from the list S and add them to the list T.
+# Reverse the list S
+# Repeat the process until list S gets emptied.
+# Example
+# S = [1,2,3,4,5,6]
+# T = []
+#
+# S = [2,3,4,5] => [5,4,3,2]
+# T = [1,6]
+#
+# S = [4,3] => [3,4]
+# T = [1,6,5,2]
+#
+# S = []
+# T = [1,6,5,2,3,4]
+# return T
+
+
+def arrange(s):
+    x = s.copy()
+    T = []
+    i = 0
+    j = len(s)-1
+    if len(s) < 2:
+        return s
+    else:
+        while i < int(len(s)/2):
+            T.append(x[i])
+            T.append(x[j])
+            i += 1
+            j -= 1
+            x = x[::-1]
+
+        if len(s) % 2 != 0:
+            mid = int(len(s)/2)
+            T.append(s[mid])
+            return T
+        else:
+            return T
+
+
+S = [2, 4, 3, 4]
+c = int(len(S)/2)
+print(S[c])
+print(arrange(S))
